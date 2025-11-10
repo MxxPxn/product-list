3a662d52471a02f46fd53c6d3482f2f59d5f519a
Max <max@max-air.local>
2025-11-09 21:10:13 -0500

    update product__grid img path

--- src/App.tsx
+++ src/App.tsx
@@ -99,9 +99,9 @@ const handleIncrease = (productId: string) => {
                 className='product__image'
                 src={
                   product.image.startsWith('./')
-                    ? import.meta.env.MODE === 'production'
-                      ? `/product-list${product.image.substring(1)}`  // Remove the dot from ./assets
-                      : new URL(product.image, import.meta.url).href
+                    // ? import.meta.env.MODE === 'production'
+                    //   ? `/product-list${product.image.substring(1)}`  // Remove the dot from ./assets
+                      ? new URL(product.image, import.meta.url).href
                     : product.image
                 }
                 alt={product.name}
